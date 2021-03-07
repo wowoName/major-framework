b1569d84ad78a5fdeeab3da52d889767f6ec3c5d
liuguangjun <1069722589@qq.com>
2021-03-07 12:48:49 +0800

    modal resize drag 边界限制

--- src/components/base-modal/index.vue
+++ src/components/base-modal/index.vue
@@ -20,6 +20,9 @@
         </div>
       </div>
     </transition>
+    <div class="resize-left" @mousedown="dragResize" data-type='left'></div>
+    <div class="resize-right" @mousedown="dragResize" data-type='right'></div>
+    <div class="resize-bottom" @mousedown="dragResize" data-type='bottom'></div>
   </div>
 </template>
 
@@ -59,9 +62,9 @@ export default {
     }
   },
   setup(props, context) {
-    const clientRect = {
-      height: document.body.clientHeight,
-      width: document.body.clientWidth
+    let clientRect = {
+      height: 0,
+      width: 0
     }
     const { emit } = context
     //header ref
@@ -84,12 +87,30 @@ export default {
       mouseDownX: 0,
       mouseDownY: 0
     }
+    //resize 数据
+    let resizeRect = {
+      x: 0,
+      y: 0
+    }
     //移动容器DOM
     let contain = null
     const dragEvent = {
       handleMove(event) {
-        const left = event.pageX - dragHandlerData.mouseDownX,
-          top = event.pageY - dragHandlerData.mouseDownY;
+        const minBoundaryValue = 46
+        let left = event.pageX - dragHandlerData.mouseDownX,
+          top = event.pageY - dragHandlerData.mouseDownY,
+          maxTop = clientRect.height - minBoundaryValue,//46为标题高度
+          maxRight = clientRect.width - minBoundaryValue;//clientRect.containerWidth
+        //禁止超过上边界
+        top < 0 && (top = 0);
+        //   (top + clientRect.containerHeight) < minBoundaryValue && (top = -clientRect.containerHeight + minBoundaryValue);
+        //禁止超过下边界
+        top > maxTop && (top = maxTop);
+        //禁止超过右侧
+        left > maxRight && (left = maxRight);
+        //禁止超过左侧
+        (left + clientRect.containerWidth) < minBoundaryValue && (left = - clientRect.containerWidth + minBoundaryValue);
+
         state.currentStyle.top = `${top}px`
         state.currentStyle.left = `${left}px`
       },
@@ -97,19 +118,22 @@ export default {
         contain.addEventListener('mousedown', function (e) {
           dragHandlerData.mouseDownX = e.pageX - modalCon.value.offsetLeft
           dragHandlerData.mouseDownY = e.pageY - modalCon.value.offsetTop
-          console.log(state.currentStyle.zIndex, props.mesh)
           if (state.currentStyle.zIndex !== props.mesh) {
             let zIndex = props.mesh
             state.currentStyle.zIndex = ++zIndex
             emit('clickPane')
           }
-
+          //当前容器大小
+          const modalConRect = modalCon.value.getBoundingClientRect()
+          clientRect.containerWidth = modalConRect.width;
+          clientRect.containerHeight = modalConRect.height;
           document.addEventListener('mousemove', dragEvent.handleMove, false)
+          document.addEventListener('mouseup', dragEvent.removeMove, false)
         })
-        document.addEventListener('mouseup', dragEvent.removeMove, false)
       },
       removeMove() {
         document.removeEventListener('mousemove', dragEvent.handleMove, false)
+        document.removeEventListener('mouseup', dragEvent.removeMove, false)
       },
     }
     const methods = {
@@ -129,7 +153,43 @@ export default {
           emit('close', props.data.id)
         }, 500)
 
-      }
+      },
+      //縮放modal
+      dragResize(event) {
+        const target = event.target;
+        let mouseDownX = event.pageX - target.offsetLeft,
+          mouseDownY = event.pageY - target.offsetTop;
+
+        resizeRect = {
+          x: mouseDownX,
+          y: mouseDownY,
+          left: target.offsetLeft,
+          width: target.parentNode.offsetWidth,
+          type: target.dataset.type
+        }
+        document.addEventListener('mousemove', setModalRect, false)
+        document.addEventListener('mouseup', removeResizeEVent, false)
+      },
+    }
+    //移除resize 相关事件
+    function removeResizeEVent() {
+      document.removeEventListener('mousemove', setModalRect, false)
+      document.addEventListener('mouseup', removeResizeEVent, false)
+    }
+    //
+    function setModalRect(e) {
+      const type = resizeRect.type
+      let width = e.pageX - resizeRect.x,
+        height = e.pageY - resizeRect.y;
+      type === 'left' && (width = resizeRect.width + resizeRect.x - e.pageX);
+
+      width < 200 && (width = 200);
+      type === 'bottom' && height < 200 && (height = 200);
+
+      type === 'left' && (state.currentStyle.left = e.pageX + 'px');
+      (['left', 'right'].includes(type)) && (state.currentStyle.width = width + 'px');
+
+      type === 'bottom' && (state.currentStyle.height = height + 'px')
     }
     //计算当前modal 样式
     function getCurrentStyleObj() {
@@ -167,6 +227,12 @@ export default {
     )
     onMounted(() => {
       state.animation = true
+      //外容器大小
+      let modalConParentRect = modalCon.value.parentNode.getBoundingClientRect();
+      clientRect = {
+        height: modalConParentRect.height,
+        width: modalConParentRect.width
+      }
       nextTick(() => {
         //计算位置
         getCurrentStyleObj()
@@ -174,6 +240,7 @@ export default {
         //初始化拖拽事件
         dragEvent.initialEvent();
       })
+
     })
     return {
       modalCon,
@@ -201,6 +268,32 @@ export default {
   height: 400px;
   border-radius: 3px;
   z-index: 1;
+  padding: 0 2px 2px 2px;
+  box-sizing: border-box;
+  .resize-left {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 3px;
+    height: 100%;
+    cursor: col-resize;
+  }
+  .resize-right {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 3px;
+    height: 100%;
+    cursor: col-resize;
+  }
+  .resize-bottom {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 3px;
+    cursor: n-resize;
+  }
   .major-modal {
     width: 100%;
     height: 100%;
